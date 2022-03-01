8719aeee084999bd524ccb000b003e933cce4742
VictorNorm <VictorNorm@users.noreply.github.com>
2022-03-01 11:13:53 +0100

    updated

---
+++ loff/src/App.tsx
@@ -0,0 +1,13 @@
+import Nav from './components/Nav';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <Nav></Nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ loff/src/components/Nav.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// export {};
+
+function Nav() {
+  return (
+    <nav>
+        <ul>
+            <li>Serier</li>
+            <li>Bassene</li>
+            <li>Om Loff</li>
+            <li>Shop</li>
+        </ul>
+        <div className='logoContainer'></div>
+    </nav>
+  )
+}
+
+export default Nav
