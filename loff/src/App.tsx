cf41d8b2bc2b0c043898910fc4d2dc8d53c61166
VictorNorm <VictorNorm@users.noreply.github.com>
2023-04-27 13:03:49 +0200

    added basename to router

--- loff/src/App.tsx
+++ loff/src/App.tsx
@@ -10,7 +10,7 @@ import CookieConsent from "react-cookie-consent";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/index.html">
       <Nav />
       <Routes>
         <Route path="/" element={<Home />}></Route>
